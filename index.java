40333029ee9d4695ee569acf0e698dd9fe5850a1
Bennertz <cristiano.bennertz@gmail.com>
2023-03-27 04:33:03 -0300

    Adição do arquivo operacoes.java.

---
+++ index.java
@@ -0,0 +1,23 @@
+package loja_coma_bem;
+
+public class index {
+	public static void main(String[] args) {
+        int xpto; // declaração de uma variável chamada ‘xpto’
+        /*como o valor não foi declarado, por padrão é zero.
+        Veremos os tipos de dados a seguir, siga adiante.
+        */
+		int decimal = 10;
+
+		//declara a variável do tipo int com valor igual a 10
+		System.out.println(decimal);
+
+		//mostra o valor da variável int
+		long inteiroLongo = 10L;
+
+		//declara a variável do tipo long com valor igual a 10
+		//o L simboliza que o valor é de uma variável do tipo long
+		System.out.println(inteiroLongo);
+
+		//mostra o valor da variável long
+	}
+}
---
+++ operacoes.java
@@ -0,0 +1,17 @@
+package loja_coma_bem;
+
+public class operacoes {
+    public static void main(String[] args) {
+        int primeiroNumero = 10;
+        int segundoNumero = 5;
+        System.out.println("Soma: " + (primeiroNumero + segundoNumero));
+        System.out.println("Subtração: " + (primeiroNumero - segundoNumero));
+        System.out.println("Multiplicação: " + (primeiroNumero * segundoNumero));
+        System.out.println("Divisão: " + (primeiroNumero / segundoNumero));
+
+        int numero = 20;
+        if (numero > 15 && numero < 30) {
+            System.out.println(numero);
+} 
+    }        
+}
