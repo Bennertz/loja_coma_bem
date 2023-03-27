54597bdce7a7ee984b5852a915d0c75ad260cb8a
Bennertz <cristiano.bennertz@gmail.com>
2023-03-27 05:47:46 -0300

    Criação de funções para separar os exemplos.

--- operacoes.java
+++ operacoes.java
@@ -1,36 +1,51 @@
 package loja_coma_bem;
 
 public class operacoes {
-    public static void main(String[] args) {
+    public static void SomaSubtraiMultiplicaDivide() {
         int primeiroNumero = 10;
         int segundoNumero = 5;
         System.out.println("Soma: " + (primeiroNumero + segundoNumero));
         System.out.println("Subtração: " + (primeiroNumero - segundoNumero));
         System.out.println("Multiplicação: " + (primeiroNumero * segundoNumero));
         System.out.println("Divisão: " + (primeiroNumero / segundoNumero));
-
-        int numero20 = 20;
-        if (numero20 > 15 && numero20 < 30) {
-            System.out.println(numero20);
+        System.out.println("------------------------------------------------------");
+    }
+    
+    public static void ELogico() {
+        int numero = 20;
+        if (numero > 15 && numero < 30) {
+            System.out.println(numero);
         }
+        System.out.println("------------------------------------------------------");
+    }
 
+    public static void ERelacional() {
         int relacional = 20;
         if (relacional == 20) {
             System.out.println("Iguais!");
         }
+        System.out.println("------------------------------------------------------");
+    }
 
-        int numero10 = 10;
-        if (numero10 <= 10) {
+    public static void MenorIgual() {
+        int numero = 10;
+        if (numero <= 10) {
             System.out.println("Menor ou igual a 10");
         }
+        System.out.println("------------------------------------------------------");
+    }
 
+    public static void MenorIgualComElse() {
         int numero20_2 = 20;
         if (numero20_2 <= 10) {
             System.out.println("Menor ou igual a 10");
         } else {
             System.out.println("Maior que 10");
         }
+        System.out.println("------------------------------------------------------");
+    }
 
+    public static void NumeroDoMes() {
         int numeroDoMes = 3;
         String mes = "mês não válido";
         switch (numeroDoMes)
@@ -75,25 +90,64 @@ public class operacoes {
             break;
         }
         System.out.println(mes);
+        System.out.println("------------------------------------------------------");
+    }
 
+    public static void FuncaoComWhile() {
         int inicializacao = 0; // inicializacao
         while (inicializacao <= 3) // condicao
         {
             System.out.println(inicializacao);
             inicializacao++; // incremento
         }
+        System.out.println("------------------------------------------------------");
+    }
 
+    public static void FuncaoComDoWhile() {
         int inicializacao_2 = 0; // inicializacao
         do
         {
             System.out.println(inicializacao_2); // instrucao
             inicializacao_2++; // incremento
         } while (inicializacao_2 <= -1); // condicao
+        System.out.println("------------------------------------------------------");
+    }
 
+    public static void FuncaoComFor() {
         for (int indice = 0; indice <= 10; indice++)
         {
             System.out.println(indice);
         }
+        System.out.println("------------------------------------------------------");
+    }
+    
+    public static void SomarNumeros() {
+        int primeiroNumero = 10;
+        int segundoNumero = 20;
+        System.out.println(primeiroNumero + segundoNumero);
+        System.out.println("------------------------------------------------------");
+    }
+        
+    public static void main(String[] args) {
+        SomaSubtraiMultiplicaDivide();
 
-    }        
+        ELogico();
+
+        ERelacional();
+        
+        MenorIgual();
+
+        MenorIgualComElse();
+        
+        NumeroDoMes();
+
+        FuncaoComWhile();
+
+        FuncaoComDoWhile();
+
+        FuncaoComFor();
+
+        SomarNumeros();
+
+    }    
 }
